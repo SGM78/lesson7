46390bc7bfbf93e825fe545fda63e1b9511375a1
Marchenkov Sergei <serguei1978@mail.ru>
2024-02-06 23:21:08 +0300

    lesson7

--- Program.cs
+++ Program.cs
@@ -28,60 +28,60 @@ class Program
 }
 
 
-// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// // Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 
-using System;
-class Program
-{
-    {
-        Console.Write("Введите значение m: ");
-        int m = int.Parse(Console.ReadLine());
+// using System;
+// class Program
+// {
+//     {
+//         Console.Write("Введите значение m: ");
+//         int m = int.Parse(Console.ReadLine());
 
-        Console.Write("Введите значение n: ");
-        int n = int.Parse(Console.ReadLine());
+//         Console.Write("Введите значение n: ");
+//         int n = int.Parse(Console.ReadLine());
 
-        int result = AckermannFunction(m, n);
+//         int result = AckermannFunction(m, n);
 
-        Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно {result}.");
-        Console.ReadLine();
-    }
+//         Console.WriteLine($"Значение функции Аккермана для ({m}, {n}) равно {result}.");
+//         Console.ReadLine();
+//     }
 
-    static int AckermannFunction(int m, int n)
-    {
-        if (m == 0)
-        {
-            return n + 1;
-        }
-        else if (n == 0)
-        {
-            return AckermannFunction(m - 1, 1);
-        }
-        else
-        {
-            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
-        }
-    }
-}
+//     static int AckermannFunction(int m, int n)
+//     {
+//         if (m == 0)
+//         {
+//             return n + 1;
+//         }
+//         else if (n == 0)
+//         {
+//             return AckermannFunction(m - 1, 1);
+//         }
+//         else
+//         {
+//             return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+//         }
+//     }
+// }
 
-// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+// // Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
 
-{
-    {
-        // Произвольный массив целых чисел
-        int[] myArray = { 1, 2, 3, 4, 5, 6, 7 };
+// {
+//     {
+//         // Произвольный массив целых чисел
+//         int[] myArray = { 1, 2, 3, 4, 5, 6, 7 };
 
-        // Вызываем рекурсивную функцию для вывода элементов массива
-        PrintArrayReversely(myArray, myArray.Length - 1);
+//         // Вызываем рекурсивную функцию для вывода элементов массива
+//         PrintArrayReversely(myArray, myArray.Length - 1);
 
-        Console.ReadLine();
-    }
+//         Console.ReadLine();
+//     }
 
-    static void PrintArrayReversely(int[] array, int currentIndex)
-    {
-        if (currentIndex >= 0)
-        {
-            Console.Write(array[currentIndex] + " ");
-            PrintArrayReversely(array, currentIndex - 1);
-        }
-    }
-}
+//     static void PrintArrayReversely(int[] array, int currentIndex)
+//     {
+//         if (currentIndex >= 0)
+//         {
+//             Console.Write(array[currentIndex] + " ");
+//             PrintArrayReversely(array, currentIndex - 1);
+//         }
+//     }
+// }
